f1645ddb2ffda9f40903c3d539d8495bbb03a6f7
Ayush Gupta <ayushgupta@Ayushs-MacBook-Air.local>
2026-06-28 13:18:33 +0530

    Initialize spring project with required dependencies

---
+++ src/main/java/dev/ayush/nexusq/NexusqApplication.java
@@ -0,0 +1,13 @@
+package dev.ayush.nexusq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ayush/nexusq/NexusqApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ayush.nexusq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
